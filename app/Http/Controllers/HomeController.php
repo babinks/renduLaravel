c9c28803989b32a5910736aa60bfaafbe181669c
babinks <55101948+babinks@users.noreply.github.com>
2020-02-12 02:37:01 +0100

    Add files via upload

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        /* TODO Récupérer tout les utilisateurs, et renvoyer la view "home.blade.php avec les utilisateurs" */
+
+        $users = User::all();
+
+
+        return view('home', ['users' => $users]);
+    }
+}
+
+
+
